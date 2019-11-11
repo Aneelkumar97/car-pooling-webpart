f468794c9a5bc825e1aa6e979f983c4103135cb6
Aneel Kumar Gajula <gakaneelkumar1997@gmail.com>
2019-11-11 17:58:42 +0530

    1. Project Initiation

---
+++ src/webparts/carPooling/CarPoolingWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'CarPoolingWebPartStrings';
+import CarPooling from './components/CarPooling';
+import { ICarPoolingProps } from './components/ICarPoolingProps';
+
+export interface ICarPoolingWebPartProps {
+  description: string;
+}
+
+export default class CarPoolingWebPart extends BaseClientSideWebPart<ICarPoolingWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ICarPoolingProps > = React.createElement(
+      CarPooling,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
